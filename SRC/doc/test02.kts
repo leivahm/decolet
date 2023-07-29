7f87fcae98e4f91723688b30871307847510d0d6
leivahm <leivahmarcelo@gmail.com>
2023-07-29 20:02:35 -0300

    adding test02

---
+++ SRC/doc/test02.kts
@@ -0,0 +1,16 @@
+fun main() {
+    /*val greeting = birthdayGreeting()
+    println(greeting)
+    println(birthdayGreeting("Ivo", 17))
+    val count: Int
+    count = 10*/
+    val count = 10
+    println("You have $count unread messages.")
+}
+
+/*fun birthdayGreeting(name: String, age: Int): String {
+fun birthdayGreeting(name: String = "Zoe", age: Int = 10): String {
+    val nameGreeting = "Happy Birthday, $name!"
+    val ageGreeting = "You are now $age years old!"
+    return "$nameGreeting\n$ageGreeting"
+}*/

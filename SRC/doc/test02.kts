25ebfd309ba4466ec82b62de13779f9e0a7ac677
leivahm <leivahmarcelo@gmail.com>
2023-07-31 20:55:21 -0300

    cleaning test02 comments

--- SRC/doc/test02.kts
+++ SRC/doc/test02.kts
@@ -2,10 +2,3 @@ fun main() {
     val count = 10
     println("You have $count unread messages.")
 }
-
-/*fun birthdayGreeting(name: String, age: Int): String {
-fun birthdayGreeting(name: String = "Zoe", age: Int = 10): String {
-    val nameGreeting = "Happy Birthday, $name!"
-    val ageGreeting = "You are now $age years old!"
-    return "$nameGreeting\n$ageGreeting"
-}*/

672e5b22241a48b73b84fb76013b0bbef5019442
leivahm <leivahmarcelo@gmail.com>
2023-08-01 00:00:27 -0300

    String and Boolean

---
+++ SRC/doc/test06.kts
@@ -0,0 +1,12 @@
+fun main() {
+    val nextMeeting = "Next meeting is: "
+    val date = "January 1"
+    val reminder = nextMeeting + date + " at work"
+    println(reminder)
+    println("Say \"hello\"")
+    val notificationsEnabled: Boolean = false
+    println("Are notifications enabled? " + notificationsEnabled)
+    var notificationsDisabled: Boolean = false
+    notificationsDisabled = true
+    println("Are notifications disabled? " + notificationsDisabled)
+}

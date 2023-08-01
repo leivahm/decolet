746700b434b3166effd4b53febdba02233b93096
leivahm <leivahmarcelo@gmail.com>
2023-07-31 22:52:51 -0300

    Giving examples of different types of variable declarations

--- SRC/doc/test02.kts
+++ SRC/doc/test02.kts
@@ -1,4 +1,9 @@
 fun main() {
     val count = 10
     println("You have $count unread messages.")
+    val mailRecievedUnread: Int
+    mailRecievedUnread= 5
+    val mailRecievedRead: Int
+    mailRecievedRead = 100
+    println("You have ${mailRecievedUnread + mailRecievedRead} total messages in your inbox.")
 }

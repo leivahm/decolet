23b3cd7533caad83cec070abd469bf6b785d3dc3
leivahm <leivahmarcelo@gmail.com>
2023-07-31 22:59:21 -0300

    Viewing the count++ method

---
+++ SRC/doc/test04.kts
@@ -0,0 +1,8 @@
+fun main() {
+    var count = 10
+    println("You have $count unread messages.")
+    count = count + 1
+    println("You have $count unread messages.")
+    count++
+    println("You have $count unread messages now.")
+}

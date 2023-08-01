6dc592f90eba0f139aa17471a5b2c84efbd14e57
leivahm <leivahmarcelo@gmail.com>
2023-07-31 23:08:11 -0300

    Working with Double type data

--- SRC/doc/test05.kts
+++ SRC/doc/test05.kts
@@ -1,4 +1,7 @@
 fun main() {
     val trip1: Double = 3.20
-    val trip2: Double = 4.10
+    val trip2 = 4.10
+    val trip3 = 1.72
+    val totalTripLength: Double = trip1 + trip2 + trip3
+    println("$totalTripLength miles left to destination")
 }

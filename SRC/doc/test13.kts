3d2d1f46b7158c77d8d7aa8d00ce9729e804fcdb
leivahm <leivahmarcelo@gmail.com>
2023-08-02 16:24:21 -0300

    Unit data type

---
+++ SRC/doc/test13.kts
@@ -0,0 +1,7 @@
+fun main() {
+    pr("Ey, hola!")
+}   // Aquí estamos pasando un string a la función pr()
+
+fun pr(mensaje: String) {
+    println(mensaje)
+}   // La función pr() devuelve el string sin "return"

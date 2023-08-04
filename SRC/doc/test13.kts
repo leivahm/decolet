951b3cb687c74e72e808eb5fc8163e40531375c4
leivahm <leivahmarcelo@gmail.com>
2023-08-03 22:53:53 -0300

    Aclaración en el tipo de dato Unit

--- SRC/doc/test13.kts
+++ SRC/doc/test13.kts
@@ -4,4 +4,7 @@ fun main() {
 
 fun pr(mensaje: String) {
     println(mensaje)
-}   // La función pr() devuelve el string sin "return"
+}   
+// La función pr() devuelve el string sin "return"
+
+// El tipo de dato aquí se llama Unit

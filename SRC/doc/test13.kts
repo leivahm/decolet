9de957e137738621801e0c7250420cb89456ca4a
leivahm <leivahmarcelo@gmail.com>
2023-08-06 15:20:02 -0300

    Clearify de concept

--- SRC/doc/test13.kts
+++ SRC/doc/test13.kts
@@ -5,6 +5,5 @@ fun main() {
 fun pr(mensaje: String) {
     println(mensaje)
 }   
-// La función pr() devuelve el string sin "return"
-
-// El tipo de dato aquí se llama Unit
+// La función pr() devuelve el string sin "return". 
+// A este tipo de dato se lo llama Unit

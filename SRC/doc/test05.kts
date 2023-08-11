6183cfa512af0dff775c5dafa898a5bb7151da4e
leivahm <leivahmarcelo@gmail.com>
2023-08-10 23:35:51 -0300

    testing test05

--- SRC/doc/test05.kts
+++ SRC/doc/test05.kts
@@ -1,5 +1,5 @@
 fun main() {
-    val trip1: Double = 3.20 // Aquí la variable se define apropiadamente
+    val trip1: Double = 5.20 // Aquí la variable se define apropiadamente
     val trip2 = 4.10    // Aquí la variable se define por inferencia de tipo
     val trip3 = 1.72    // Aquí también
     val totalTripLength: Double = trip1 + trip2 + trip3

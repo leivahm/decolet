e2646c7b9f19911fdc064c50a453af3b96e9bf94
leivahm <leivahmarcelo@gmail.com>
2023-08-16 14:25:19 -0300

    comment test03

--- SRC/doc/test03.kts
+++ SRC/doc/test03.kts
@@ -2,6 +2,6 @@ fun main () {
     var cartTotal = 0
     //cartTotal = 20
     println("Total: $cartTotal")
-    cartTotal = 20
+    cartTotal = 20 // Modifica el valor de la variable 
 	println("Total: $cartTotal")
 }

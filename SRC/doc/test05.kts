7f755d174dc11179b739524f08e190395e50c028
leivahm <leivahmarcelo@gmail.com>
2023-08-18 21:42:14 -0300

    commenting

--- SRC/doc/test05.kts
+++ SRC/doc/test05.kts
@@ -1,7 +1,7 @@
 fun main() {
     val trip1: Double = 5.20 // Aquí la variable se define apropiadamente
     val trip2 = 4.10    // Aquí la variable se define por inferencia de tipo
-    val trip3 = 1.72    // Aquí también
+    val trip3 = 1.72    // Aquí también se define por inferencia de tipo
     val totalTripLength: Double = trip1 + trip2 + trip3 // Aquí tmb se define la variable apropiadamente
     println("$totalTripLength miles left to destination")
 }

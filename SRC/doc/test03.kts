a28157eba1d59d5733f3890e4ecb9fa7b02ffb87
leivahm <leivahmarcelo@gmail.com>
2023-08-22 20:04:20 -0300

    adding another variable

--- SRC/doc/test03.kts
+++ SRC/doc/test03.kts
@@ -1,7 +1,8 @@
 fun main () {
     var cartTotal = 0
+    plus = 20
     //cartTotal = 20
     println("Total: $cartTotal")
-    cartTotal = 20 // Modifica el valor de la variable 
+    cartTotal = 20 + plus // Modifica el valor de la variable 
 	println("Total: $cartTotal")
 }
